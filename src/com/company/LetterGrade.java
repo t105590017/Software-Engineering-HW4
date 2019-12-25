d1d11e061dad72ba3ba447a54d41c8f112b3e5b1
t105590017 <t105590017@ntut.org.tw>
2019-12-25 15:36:14 +0800

    FIRST COMMIT

---
+++ src/com/company/LetterGrade.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class LetterGrade {
+    public static char letterGrade(int score) {
+        char grade;
+        if (score <0 || score > 100)
+            grade = 'X';
+        else if (score >= 90)
+            grade = 'A';
+        else if (score >= 80)
+            grade = 'B';
+        else if (score >= 70)
+            grade = 'C';
+        else if (score >= 60)
+            grade = 'D';
+        else
+            grade ='F';
+        return grade;
+    }
+}
