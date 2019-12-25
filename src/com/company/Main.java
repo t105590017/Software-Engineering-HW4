a12af847ca7f13a74c28505bc30db06cac0988a4
t105590017 <t105590017@ntut.org.tw>
2019-12-25 15:41:39 +0800

    FIRST main.java COMMIT

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static com.company.LetterGrade.letterGrade;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
